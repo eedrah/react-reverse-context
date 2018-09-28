bffe6a7f16134e50e75f654ab7dd1d4ea944309d
Eedrah <eedrah@users.noreply.github.com>
2018-09-28 15:20:43 -0500

    feat: add first version

---
+++ index.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import { func, any, element } from 'prop-types'
+
+class InnerReverseProvider extends React.Component {
+  constructor(props) {
+    super(props)
+    props.setValue(props.value)
+  }
+  render() {
+    return null
+  }
+}
+InnerReverseProvider.propTypes = {
+  setValue: func.isRequired,
+  value: any.isRequired,
+}
+
+const ReverseProvider = ({ value, ContextConsumer }) => (
+  <ContextConsumer>
+    {setValue => <InnerReverseProvider setValue={setValue} value={value} />}
+  </ContextConsumer>
+)
+ReverseProvider.propTypes = {
+  value: any.isRequired,
+  ContextConsumer: element.isRequired,
+}
+
+class ReverseConsumer extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: props.defaultValue,
+    }
+    this.changeValue = this.changeValue.bind(this)
+  }
+  changeValue(value) {
+    this.setState({ value })
+  }
+  render() {
+    const ContextProvider = this.props.ContextProvider
+    return (
+      <ContextProvider value={this.changeValue}>
+        {this.props.children(this.state.value)}
+      </ContextProvider>
+    )
+  }
+}
+ReverseConsumer.propTypes = {
+  defaultValue: any,
+  ContextProvider: element.isRequired,
+  children: func.isRequired,
+}
+
+const createReverseContext = defaultValue => {
+  const context = React.createContext()
+
+  const Consumer = ({ children }) => (
+    <ReverseConsumer
+      ContextProvider={context.Provider}
+      defaultValue={defaultValue}
+    >
+      {children}
+    </ReverseConsumer>
+  )
+  Consumer.propTypes = { children: func.isRequired }
+
+  const Provider = ({ value }) => (
+    <ReverseProvider ContextConsumer={context.Consumer} value={value} />
+  )
+  Provider.propTypes = { value: any.isRequired }
+
+  return { Consumer, Provider }
+}
+
+export { createReverseContext }
