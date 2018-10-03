8b3807f449a77add070ca3e51d1568e38eefcb2f
Eedrah <eedrah@users.noreply.github.com>
2018-10-03 10:26:48 -0500

    init create-react-library@2.6.6

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,37 @@
-import external from 'rollup-plugin-peer-deps-external'
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
 import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
+import external from 'rollup-plugin-peer-deps-external'
+import postcss from 'rollup-plugin-postcss'
+import resolve from 'rollup-plugin-node-resolve'
+import url from 'rollup-plugin-url'
+
 import pkg from './package.json'
 
-export default [
-  {
-    input: 'src/index.js',
-    output: [
-      { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'es' },
-    ],
-    plugins: [
-      external(),
-      babel({
-        exclude: 'node_modules/**',
-      }),
-      resolve(),
-      commonjs({
-        namedExports: {
-          'prop-types': ['func', 'any', 'object'],
-        },
-      }),
-    ],
-  },
-]
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true
+    }
+  ],
+  plugins: [
+    external(),
+    postcss({
+      modules: true
+    }),
+    url(),
+    babel({
+      exclude: 'node_modules/**',
+      plugins: [ 'external-helpers' ]
+    }),
+    resolve(),
+    commonjs()
+  ]
+}
